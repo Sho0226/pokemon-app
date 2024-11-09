b57ff10ab725321fe631dc528e0ed6094fef7507
sho0226 <s1f102201616@iniad.org>
2024-11-09 15:21:16 +0900

    add: fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
+  const InitialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(InitialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
   return <div className="App"></div>;
 }
 
